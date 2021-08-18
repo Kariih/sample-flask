b3a6d32fb2c8f44791285c36a5df3a46c6e90dd1
Kari Helene <kbekkelund@zendesk.com>
2021-08-18 17:21:24 +0100

    add files

--- app.py
+++ app.py
@@ -7,3 +7,11 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return render_template("index.html")
+
+@app.route('/my_data/', methods=['POST'])
+def work_with_data():
+    print request.form
+
+@app.route('/<user>')
+def user(user):
+    return render_template("displaydata.html", data=user)
