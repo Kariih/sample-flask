dde02c006de0b12761141f38ad98d07685abc63d
Kari Helene <kbekkelund@zendesk.com>
2021-08-18 21:51:54 +0100

    changr files

--- app.py
+++ app.py
@@ -1,17 +1,24 @@
 from flask import Flask
 from flask import render_template
+from flask import request
 
 app = Flask(__name__)
 
-
 @app.route("/")
 def hello_world():
-    return render_template("index.html")
+    return render_template("displaydata.html", data="Hi there")
 
-@app.route('/<user>')
-def user(user):
-    return render_template("displaydata.html", data=request)
+@app.route('/read-data')
+def read_data():
+    f = open("datafile", "r")
+    return render_template("displaydata.html", data=f.read())
 
 @app.route('/endpoint', methods = ['POST'])
 def endpoint_post():
-    return render_template("displaydata.html", data=user)
+    data_rec=request.get_data().decode("utf-8")
+    app.logger.debug('Headers: %s', request.headers)
+    app.logger.debug('Body: %s', data_rec)
+    f = open('datafile', 'a')
+    f.write(data_rec + ' -- ')
+    f.close()
+    return render_template("displaydata.html", data="success")
