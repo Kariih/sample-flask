80fe008cb3a9f62ab1ec7058ca37ac8e2b4a7e72
Kari Helene <kbekkelund@zendesk.com>
2021-08-18 20:18:37 +0100

    changr files

--- app.py
+++ app.py
@@ -8,10 +8,6 @@ app = Flask(__name__)
 def hello_world():
     return render_template("index.html")
 
-@app.route('/my_data/', methods=['POST'])
-def work_with_data():
-    print request.form
-
 @app.route('/<user>')
 def user(user):
     return render_template("displaydata.html", data=user)
